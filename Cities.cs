a8927e388a977d88447cee4003b876effbed62e9
User <User@DESKTOP-DJ5CCR3>
2024-04-22 18:17:49 +0300

    Добавьте файлы проекта.

---
+++ Cities.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Presentation;
+using DocumentFormat.OpenXml.Spreadsheet;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace WeatherApp
+{
+    internal class Cities
+    {
+        public string City_Name_eng { get; set; }
+        public string City_Name_rus { get; set; }
+        public double longityde { get; set; }
+        public double latitude { get; set; }
+        public string Country { get; set; }
+
+        public static string Path(string xlsxpath_ar)
+        {
+            return String.Concat("D:\\Visual Studio\\Проекты\\C#\\WeatherApp\\", xlsxpath_ar);
+        }
+
+        public static IEnumerable<Cities> CitiesEnumerate(string xlsxpath_ar, int number_of_list = 1)
+        {
+            var xlsxpath = Path(xlsxpath_ar);
+            // Открываем книгу
+            using (var workbook = new XLWorkbook(xlsxpath))
+            {
+                // Берем в ней первый лист
+                var worksheet = workbook.Worksheet(number_of_list);
+
+                int row = 1;
+                
+                while (!worksheet.Cell(row, 1).IsEmpty())
+                {   
+                    
+                    var metric = new Cities
+                    {
+                        City_Name_eng = worksheet.Cell(row, 1).GetValue<string>(),
+                        City_Name_rus = worksheet.Cell(row, 2).GetValue<string>(),
+                        longityde = worksheet.Cell(row, 3).GetValue<double>(),
+                        latitude = worksheet.Cell(row, 4).GetValue<double>(),
+                        Country = worksheet.Cell(row, 5).GetValue<string>(),
+                    };
+
+                    row++;
+
+                    yield return metric;
+                }
+                
+            }
+        
+        }
+
+        internal void Add(string path_ar, int number_of_list)
+        {
+            var path = Path(path_ar);
+
+            try
+            {
+
+                using (var workbook = new XLWorkbook(path))
+                {
+                    // Берем в ней первый лист
+                    var worksheet = workbook.Worksheet(number_of_list);
+
+                    int row = 1;
+
+                    while (!worksheet.Cell(row, 1).IsEmpty()) {
+
+                        if (worksheet.Cell(row, 1).GetValue<string>() == this.City_Name_eng) {
+                            MessageBox.Show("Город уже добавлен!");
+                            break; 
+                        }
+
+                        row++; 
+                    }
+
+
+                    worksheet.Cell(row, 1).Value = this.City_Name_eng.ToString();
+                    worksheet.Cell(row, 2).Value = this.City_Name_rus.ToString();
+                    worksheet.Cell(row, 3).Value = this.longityde.ToString();
+                    worksheet.Cell(row, 4).Value = this.latitude.ToString();
+                    worksheet.Cell(row, 5).Value = this.Country.ToString();
+
+                    workbook.Save();
+
+                }
+                
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.ToString());
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return $"{City_Name_rus}";
+        }
+
+        internal static async Task DeleteRow()
+        {
+            await Task.Run(() =>
+            {
+                using (var workbook = new XLWorkbook(Cities.Path("cities_list.xlsx")))
+                {
+                    var worksheet = workbook.Worksheet(0);
+
+                    // Удалить строку
+                    worksheet.Row(1).Delete();
+
+
+                    workbook.SaveAs(Cities.Path("cities_list.xlsx"));
+                }
+            });
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Office.Interop.Excel;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+using System.Diagnostics;
+using System.Threading;
+using System.Net.Mail;
+using DocumentFormat.OpenXml.Vml;
+using System.Collections.ObjectModel;
+using ClosedXML.Excel;
+
+namespace WeatherApp
+{
+    public partial class MainWindow /*: Window*/
+    {
+        Cities value;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+            this.Loaded += MainWindow_Loaded;
+            
+        }
+
+        private void Cities_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+
+                if (sender is System.Windows.Controls.ComboBox)
+                {
+                    value = Cities_ComboBox.SelectedValue as Cities;
+                }
+                else
+                {
+                    value = Cities_ListBox.SelectedValue as Cities;
+                }
+
+                if (!(value is Cities)) return;
+
+                Chosen_City.Text = value.City_Name_rus;
+
+                string UrlString = "https://api.openweathermap.org/data/2.5/weather?q=" +
+                    value.City_Name_eng + "," +
+                    value.Country + "&lon=" +
+                    value.longityde.ToString() + "&lat=" +
+                    value.latitude.ToString() + "&appid=4ef16c1091ff58f06bdf640b80d809f1&mode=xml&units=metric&lang=ru";
+
+                XmlReader xmlread = XmlReader.Create(UrlString);
+
+                while (xmlread.Read())
+                {
+                    if ((xmlread.NodeType == XmlNodeType.Element) && (xmlread.Name == "temperature"))
+                    {
+                        if (xmlread.HasAttributes)
+                            Output.Text = xmlread.GetAttribute("value");
+                    }
+                }
+           
+        }
+
+        private void Button_Add_Click(object sender, RoutedEventArgs e)
+        {
+            value.Add("cities_list.xlsx", 2);
+            // Может что-то менее ресурсозатратное
+            Cities_ListBox.ItemsSource = Cities.CitiesEnumerate("cities_list.xlsx", 2);
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+                Cities_ComboBox.ItemsSource = Cities.CitiesEnumerate("cities_list.xlsx");
+                Cities_ListBox.ItemsSource = Cities.CitiesEnumerate("cities_list.xlsx", 2);
+
+                Cities_ListBox.SelectedIndex = 0;
+
+                if (value == null)
+                    Cities_ComboBox.SelectedIndex = 217;
+        }
+
+        private async void Remove_MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show("Ага захотел!");
+            //
+            //Task task = new Task.Run(() =>
+            //{
+            //    
+            //    using (var workbook = new XLWorkbook(Cities.Path("cities_list.xlsx")))
+            //    {
+            //        var worksheet = workbook.Worksheet(0);
+            //
+            //        worksheet.Row(1).Delete();
+            //
+            //        workbook.SaveAs(Cities.Path("cities_list.xlsx"));
+            //    }
+            //});
+            await Cities.DeleteRow();
+        }
+    }
+    
+    
+}
